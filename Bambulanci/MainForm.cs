6a29e423f9bc10c175204ca19f446174f3082072
jfuerst98 <jfuerst98@gmail.com>
2020-08-22 15:57:53 +0200

    Code structure improvements. Beginning of form paralelism.

---
+++ Bambulanci/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bambulanci
+{
+	public enum GameState { Intro, HostSelect, HostWaiting, ClientWaiting, HostWaitingRoom, ClientWaitingRoom, ClientSearch }
+
+	public partial class formBambulanci : Form
+	{
+		private GameState currentGameState;
+		private Client client;
+		private Host host;
+
+		public formBambulanci()
+		{
+			InitializeComponent();
+			client = new Client(this);
+			host = new Host(this);
+			ChangeGameState(GameState.Intro);
+		}
+
+		private void DisableControl(Control c)
+		{
+			c.Enabled = false;
+			c.Visible = false;
+		}
+
+		private void EnableControl(Control c)
+		{
+			c.Enabled = true;
+			c.Visible = true;
+		}
+
+		public void ChangeGameState(GameState newState)
+		{
+			currentGameState = newState;
+			switch (currentGameState)
+			{
+				case GameState.Intro:
+						EnableControl(bCreateGame);
+						EnableControl(bConnect);
+						break;
+				case GameState.HostSelect:
+					DisableControl(bCreateGame);
+					DisableControl(bConnect);
+					EnableControl(lBNumOfPlayers);
+					EnableControl(bCreateGame2);
+					EnableControl(nListenPort);
+					break;
+				case GameState.HostWaiting:
+					DisableControl(lBNumOfPlayers);
+					DisableControl(bCreateGame2);
+					DisableControl(nListenPort);
+					EnableControl(lWaiting);
+					break;
+				case GameState.ClientWaiting:
+					DisableControl(nHostPort);
+					DisableControl(bRefreshServers);
+					DisableControl(lBServers);
+					DisableControl(bLogin);
+					EnableControl(lWaiting);
+					client.MoveSelfToWaitingRoom();
+					break;
+				case GameState.HostWaitingRoom:
+					DisableControl(lWaiting);
+					//vsem klientum napisu, ze se maji presunout do waiting room + pridam jejich ID (pozice)...
+					host.MoveClientsToWaitingRoom();
+					//chci nejakou grafiku....
+					break;
+				case GameState.ClientWaitingRoom:
+					DisableControl(lWaiting);
+					break;
+				case GameState.ClientSearch:
+					DisableControl(bCreateGame);
+					DisableControl(bConnect);
+					EnableControl(lBServers);
+					EnableControl(bLogin);
+					EnableControl(nHostPort);
+					EnableControl(bRefreshServers);
+					break;
+				default:
+					break;
+			}
+		}
+		
+		private void bCreateGame_Click(object sender, EventArgs e)
+		{
+			ChangeGameState(GameState.HostSelect);
+		}
+
+		private void bCreateGame2_Click(object sender, EventArgs e)
+		{
+			ChangeGameState(GameState.HostWaiting);
+			int numOfPlayers = lBNumOfPlayers.SelectedIndex + 1;
+			//nasledujici provest paralelne***********************************************************
+			//Thread t = new Thread(() => StartHost(numOfPlayers, (int)nListenPort.Value));
+			//t.Start();
+
+
+			host.StartHost(numOfPlayers, (int)nListenPort.Value);
+			/*
+			backgroundWorker1 = new BackgroundWorker();
+			backgroundWorker1.DoWork += new DoWorkEventHandler(BackgroundWorker1_DoWork);
+			backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BackgroundWorker1_RunWorkerCompleted);
+
+			backgroundWorker1.RunWorkerAsync((numOfPlayers, (int)nListenPort.Value));
+			*/
+		}
+
+		private void bConnect_Click(object sender, EventArgs e)
+		{
+			ChangeGameState(GameState.ClientSearch);
+			client.StartClient();
+		}
+
+		private void bLogin_Click(object sender, EventArgs e)
+		{
+			string[] tokens = lBServers.SelectedItem.ToString().Split(':');
+			IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse(tokens[0]), int.Parse(tokens[1]));
+			client.clientSocket.SendTo(new byte[] { (byte)Command.Login }, serverEP);
+			ChangeGameState(GameState.ClientWaiting);
+		}
+		private void bRefreshServers_Click(object sender, EventArgs e)
+		{
+			lBServers.Items.Clear();
+			int hostPort = (int)nHostPort.Value;
+			IPEndPoint broadcastEP = new IPEndPoint(IPAddress.Broadcast, hostPort);
+			client.clientSocket.SendTo(new byte[] { (byte)Command.FindServers }, broadcastEP);
+
+			Console.WriteLine("broadcast sent");
+
+			byte[] serverInfo = new byte[1024]; //1024???
+
+			//find all servers---------------------------------------melo by se dit paralelne
+			for (int i = 0; i < 1; i++) //dokud si nevyberu, vyhledavam...
+			{
+				int receivedAmount = client.clientSocket.Receive(serverInfo);
+				string serverInfoString = Encoding.ASCII.GetString(serverInfo);
+				lBServers.Items.Add(serverInfoString);
+			}
+
+		}
+
+	}
+}
---
+++ Bambulanci/Networking.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Bambulanci
+{
+	
+	//networking---------------------
+	enum Command { Login, Logout, FindServers, MoveToWaitingRoom }
+	struct ClientInfo
+	{
+		public int id;
+		//private string name; //bez newline?
+		//private Color color; //?
+		public IPEndPoint ipEndPoint;
+	}
+
+	class Host
+	{
+		formBambulanci form;
+		public Host(formBambulanci form)
+		{
+			this.form = form;
+		}
+
+		List<ClientInfo> clientList;
+		UdpClient host;
+		public void StartHost(int numOfPlayers, int listenPort)
+		{
+			clientList = new List<ClientInfo>(); //size is known...could be array
+			int id = 1; //0 is host
+
+			IPAddress hostIP = null; //might not work in case of multiple IPv4 addresses
+			IPAddress[] addresses = Dns.GetHostAddresses(Dns.GetHostName());
+			foreach (var address in addresses)
+			{
+				if (address.AddressFamily == AddressFamily.InterNetwork)
+				{
+					hostIP = address;
+					break;
+				}
+			}
+			host = new UdpClient(new IPEndPoint(hostIP, listenPort));
+
+			IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+			while (clientList.Count < numOfPlayers)
+			{
+				byte[] data = host.Receive(ref clientEP);
+
+				//parser-----
+				Command command = (Command)data[0];
+
+				switch (command)
+				{
+					case Command.Login:
+						Console.WriteLine($"New client: {clientEP}");
+						ClientInfo clientInfo = new ClientInfo() { id = id, ipEndPoint = clientEP };
+						clientList.Add(clientInfo);
+						id++; //muze zpusobit potize v pripade reconnection
+						break;
+					case Command.Logout:
+						break;
+					case Command.FindServers:
+						byte[] serverInfo = Encoding.ASCII.GetBytes(host.Client.LocalEndPoint.ToString()); //"ping"
+						host.Send(serverInfo, serverInfo.Length, clientEP);
+						Console.WriteLine("broadcast received");
+						break;
+					default:
+						break;
+				}
+			}
+			form.ChangeGameState(GameState.HostWaitingRoom);
+		}
+
+		public void MoveClientsToWaitingRoom()
+		{
+			foreach (var client in clientList)
+			{
+				byte[] message = { (byte)Command.MoveToWaitingRoom, (byte)client.id }; //INT AS BYTES--------------------------?????
+				host.Send(message, message.Length, client.ipEndPoint);
+			}
+		}
+
+
+		private BackgroundWorker backgroundWorker1;
+		private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+		{
+			(int numOfPlayers, int nListenPortValue) = (ValueTuple<int, int>)e.Argument;
+
+			StartHost(numOfPlayers, nListenPortValue);
+
+		}
+		private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+			//nic
+		}
+
+	}
+
+	class Client
+	{
+		private formBambulanci form;
+		public Client(formBambulanci form)
+		{
+			this.form = form;
+		}
+		public Socket clientSocket; //asi bych chtel spise private??--
+
+		public void MoveSelfToWaitingRoom()
+		{
+			byte[] data = new byte[1024]; //1024???
+			while (true)
+			{
+				clientSocket.Receive(data);
+				Command command = (Command)data[0];
+				if (command == Command.MoveToWaitingRoom)
+				{
+					byte id = (byte)data[1];
+					Console.WriteLine($"client id: {id}");
+					form.ChangeGameState(GameState.ClientWaitingRoom);
+				}
+			}
+		}
+
+		public void StartClient()
+		{
+			clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			clientSocket.EnableBroadcast = true;
+		}
+
+	}
+
+}
